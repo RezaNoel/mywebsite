79b05a1b7b4a5bb14ae424879a79c633bfbcefbc
rezanoel <rezatvg112@gmail.com>
2026-06-13 14:36:44 +0330

    Keep only dist folder

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: '/mywebsite/', // ⚠️ خیلی مهم: اسم ریپازیتوری گیت‌هابت باید بین دو تا اسلش باشد
-})
