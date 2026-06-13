43c0d5f326a90ded1c6413f03f7bdc4bbd4b41d5
rezanoel <rezatvg112@gmail.com>
2026-06-13 14:13:25 +0330

    fix css

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mywebsite/',
+  base: '/mywebsite/', // ⚠️ خیلی مهم: اسم ریپازیتوری گیت‌هابت باید بین دو تا اسلش باشد
 })
